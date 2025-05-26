c95cca894f8bf3694b514960238671935994a7e7
Stephon Proctor <stephon.proctor@gmail.com>
2025-05-25 21:44:46 -0400

    name changes
    naming updates

--- src/uiManager.js
+++ src/uiManager.js
@@ -172,7 +172,7 @@ export class UIManager {
       <div class="welcome-message">
         <div class="welcome-header">
           <span class="icon">🤖</span>
-          <h3>Welcome to CAia</h3>
+          <h3>Welcome to CAiA</h3>
         </div>
         <p>I can help you analyze patient data, answer questions about medications, vitals, conditions, and more. Ask me anything about this patient!</p>
         <div class="quick-start">
